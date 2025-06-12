1ba23152e8275c65c1aab4d8d6cecf38da4902c7
Ntand42 <ntandom2003@gmail.com>
2025-06-12 08:56:10 +0000

    Implement print_chessboard function to print a chessboard

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,6 +21,8 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
+void print_chessboard(char (*a)[8]);
+
 
 char *_memset(char *s, char b, unsigned int n);
 
